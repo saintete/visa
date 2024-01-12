db07cc46583f91b3b1de95d1fe2765b7657ad208
jnnjeude <njeudeuj@gmail.com>
2024-01-12 02:22:26 +0000

    version2

--- deploy.py
+++ deploy.py
@@ -1,6 +1,6 @@
 # Deploying a python web appliecho
-echo "Hello world" 
+#echo "Hello world" 
 #= shell scripts
-python('Hello World')
+#python('Hello World')
 #= python script
-
+print('Hello World')
